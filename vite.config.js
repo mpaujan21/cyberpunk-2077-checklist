c098287516b9e1a26806bcf59aafd065492f98c8
m.fauzan <m.fauzan@kemlu.go.id>
2025-01-01 22:39:46 +0700

    add vite config

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,8 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [
     vue(),
     vueDevTools(),
